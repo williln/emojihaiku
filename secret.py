74b5bc2285e4f7086c0f0ca687c1872100aed237
Lacey Williams Henschel <laceynwilliams@gmail.com>
2015-10-19 18:54:22 -0700

    Removing files again

--- secret.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'IbAnpLZgNHgwntW3suVzMbvsb'
-consumer_secret = 'f40YzCpNNytpeChTep8GSrSL4RS29xEblIRuiBPKbFSE8U80zt'
-access_key = '3285702793-JmCguPzynrNJCmsk9IHhPDG8QFOZJdkEzCeRde3'
-access_secret = 'dv2ERXdSEjiB0OJpFkfKMULEUrakz8cgcH1XTTj2ILpgH'
